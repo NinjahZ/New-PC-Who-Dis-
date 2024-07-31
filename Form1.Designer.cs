c887dcd01c629f48ce8268f0ce78cf6518ad7eca
SkyFlare <5196167+NinjahZ@users.noreply.github.com>
2024-07-31 17:51:57 +1200

    Initial Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace NinjahZ_Tools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.DownloadProgressBar = new System.Windows.Forms.ProgressBar();
+            this.NoteLabel = new System.Windows.Forms.Label();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.fileProgressBar = new System.Windows.Forms.ProgressBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TextBoxSystemInfo = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Location = new System.Drawing.Point(7, 448);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(512, 23);
+            this.DownloadButton.TabIndex = 14;
+            this.DownloadButton.Text = "Download";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // DownloadProgressBar
+            // 
+            this.DownloadProgressBar.Location = new System.Drawing.Point(7, 501);
+            this.DownloadProgressBar.Name = "DownloadProgressBar";
+            this.DownloadProgressBar.Size = new System.Drawing.Size(512, 18);
+            this.DownloadProgressBar.TabIndex = 15;
+            // 
+            // NoteLabel
+            // 
+            this.NoteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NoteLabel.Location = new System.Drawing.Point(6, 338);
+            this.NoteLabel.Name = "NoteLabel";
+            this.NoteLabel.Size = new System.Drawing.Size(513, 73);
+            this.NoteLabel.TabIndex = 16;
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(6, 432);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(47, 13);
+            this.StatusLabel.TabIndex = 17;
+            this.StatusLabel.Text = "Awaiting";
+            // 
+            // fileProgressBar
+            // 
+            this.fileProgressBar.Location = new System.Drawing.Point(6, 477);
+            this.fileProgressBar.Name = "fileProgressBar";
+            this.fileProgressBar.Size = new System.Drawing.Size(512, 18);
+            this.fileProgressBar.TabIndex = 18;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(532, 551);
+            this.tabControl1.TabIndex = 19;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.DownloadProgressBar);
+            this.tabPage1.Controls.Add(this.fileProgressBar);
+            this.tabPage1.Controls.Add(this.StatusLabel);
+            this.tabPage1.Controls.Add(this.DownloadButton);
+            this.tabPage1.Controls.Add(this.NoteLabel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(524, 525);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Downloads";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.TextBoxSystemInfo);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(524, 525);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "System Information";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxSystemInfo
+            // 
+            this.TextBoxSystemInfo.Location = new System.Drawing.Point(6, 6);
+            this.TextBoxSystemInfo.Multiline = true;
+            this.TextBoxSystemInfo.Name = "TextBoxSystemInfo";
+            this.TextBoxSystemInfo.ReadOnly = true;
+            this.TextBoxSystemInfo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.TextBoxSystemInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxSystemInfo.Size = new System.Drawing.Size(512, 513);
+            this.TextBoxSystemInfo.TabIndex = 0;
+            this.TextBoxSystemInfo.TabStop = false;
+            this.TextBoxSystemInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 575);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "New PC Who Dis?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.ProgressBar DownloadProgressBar;
+        private System.Windows.Forms.Label NoteLabel;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.ProgressBar fileProgressBar;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox TextBoxSystemInfo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Linq;
+
+namespace NinjahZ_Tools
+{
+    public partial class Form1 : Form
+    {
+        private AppConfig config;
+        private string configFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "config.cfg");
+
+        public Form1()
+        {
+            InitializeComponent();
+            config = AppConfig.Load(configFilePath);
+            InitializeCheckBoxes();
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = sender as CheckBox;
+            if (checkBox != null)
+            {
+                // Handle the event when a checkbox is checked or unchecked
+                // You can customize the behavior here based on the checkbox that triggered the event
+                if (checkBox.Checked)
+                {
+                    // Logic when the checkbox is checked
+                    NoteLabel.Text = $"{checkBox.Text} is checked.";
+                }
+                else
+                {
+                    // Logic when the checkbox is unchecked
+                    NoteLabel.Text = $"{checkBox.Text} is unchecked.";
+                }
+            }
+        }
+
+        private void InitializeCheckBoxes()
+        {
+            FlowLayoutPanel flowLayoutPanel = new FlowLayoutPanel
+            {
+                Dock = DockStyle.Fill, // Makes the panel fill the entire tab page
+                AutoScroll = true,     // Adds a scrollbar if there are too many checkboxes
+                FlowDirection = FlowDirection.TopDown, // Arrange controls from top to bottom
+            };
+            tabPage1.Controls.Add(flowLayoutPanel);
+
+            foreach (var entry in config.CheckBoxes)
+            {
+                CheckBox checkBox = new CheckBox
+                {
+                    Text = entry.Text,
+                    AutoSize = true,
+                };
+                checkBox.CheckedChanged += CheckBox_CheckedChanged;
+                flowLayoutPanel.Controls.Add(checkBox); // Add the CheckBox to the FlowLayoutPanel
+            }
+        }
+
+        private async void DownloadButton_Click(object sender, EventArgs e)
+        {
+            List<Task> downloadTasks = new List<Task>();
+            int checkedCount = 0;
+
+            foreach (var checkBoxConfig in config.CheckBoxes)
+            {
+                var checkBox = Controls.OfType<CheckBox>().FirstOrDefault(cb => cb.Text == checkBoxConfig.Text);
+                if (checkBox != null && checkBox.Checked)
+                {
+                    checkedCount += checkBoxConfig.Urls.Count;
+
+                    foreach (var url in checkBoxConfig.Urls)
+                    {
+                        downloadTasks.Add(DownloadFileAsync(checkBox, url));
+                    }
+                }
+            }
+
+            if (checkedCount > 0)
+            {
+                DownloadProgressBar.Maximum = checkedCount;
+                DownloadProgressBar.Value = 0;
+
+                try
+                {
+                    await Task.WhenAll(downloadTasks);
+                    MessageBox.Show("Download completed!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No files selected for download.");
+            }
+        }
+
+        private async Task DownloadFileAsync(CheckBox checkBox, string url)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                    response.EnsureSuccessStatusCode();
+
+                    var fileName = Path.GetFileName(new Uri(url).LocalPath);
+
+                    // Remove "Download" from the checkbox text and replace spaces with underscores
+                    var folderName = checkBox.Text.Replace("Download ", "").Trim().Replace(" ", "_");
+                    var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    var folderPath = Path.Combine(desktopPath, folderName);
+
+                    Directory.CreateDirectory(folderPath);
+
+                    var savePath = Path.Combine(folderPath, fileName);
+
+                    Invoke(new Action(() =>
+                    {
+                        StatusLabel.Text = $"Downloading: {fileName}";
+                        fileProgressBar.Value = 0;
+                        fileProgressBar.Maximum = 100; // Percentage-based updates
+                    }));
+
+                    using (var fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        var buffer = new byte[8192];
+                        var totalBytesRead = 0L;
+                        var bytesRead = 0;
+                        var totalBytes = response.Content.Headers.ContentLength.GetValueOrDefault(-1);
+
+                        if (totalBytes > 0)
+                        {
+                            Invoke(new Action(() => fileProgressBar.Maximum = (int)totalBytes));
+                        }
+
+                        using (var responseStream = await response.Content.ReadAsStreamAsync())
+                        {
+                            while ((bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                            {
+                                await fileStream.WriteAsync(buffer, 0, bytesRead);
+                                totalBytesRead += bytesRead;
+
+                                // Update progress
+                                if (totalBytes != -1)
+                                {
+                                    int progressPercentage = (int)((totalBytesRead * 100) / totalBytes);
+                                    Invoke(new Action(() =>
+                                    {
+                                        fileProgressBar.Value = progressPercentage;
+                                        StatusLabel.Text = $"Downloading: {fileName} - {progressPercentage}%";
+                                    }));
+                                }
+                                else
+                                {
+                                    Invoke(new Action(() =>
+                                    {
+                                        fileProgressBar.Value = (int)totalBytesRead;
+                                        StatusLabel.Text = $"Downloading: {fileName}";
+                                    }));
+                                }
+                            }
+                        }
+                    }
+
+                    Invoke(new Action(() =>
+                    {
+                        StatusLabel.Text = $"{fileName} download complete";
+                        DownloadProgressBar.Value++;
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                Invoke(new Action(() =>
+                {
+                    StatusLabel.Text = $"Error downloading {url}: {ex.Message}";
+                    fileProgressBar.Value = 0;
+                }));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NoteLabel.Text = string.Empty;
+
+            string tempFilePath = Path.GetTempFileName();
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                FileName = "dxdiag",
+                Arguments = $"/t \"{tempFilePath}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process process = Process.Start(processStartInfo))
+            {
+                process.WaitForExit();
+            }
+
+            if (File.Exists(tempFilePath))
+            {
+                try
+                {
+                    TextBoxSystemInfo.Text = File.ReadAllText(tempFilePath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error reading system information: {ex.Message}");
+                }
+                finally
+                {
+                    File.Delete(tempFilePath);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Failed to generate system information.");
+            }
+        }
+    }
+
+    public class AppConfig
+    {
+        public List<CheckBoxConfig> CheckBoxes { get; set; }
+
+        public static AppConfig Load(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                // Create default configuration if the file does not exist
+                var defaultConfig = new AppConfig
+                {
+                    CheckBoxes = new List<CheckBoxConfig>
+                    {
+                        new CheckBoxConfig { Text = "X570F", Urls = new List<string>
+                        {
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/04LAN/DRV_LAN_Intel_I211_SZ_TSD_W10_64_V121897_20200717R.zip?model=ROG%20STRIX%20X570-F%20GAMING",
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/Socket%20AM5/PRIME%20X670-P/DRV_Chipset_AMD_AM5_SZ-TSD_W11_64_V60516221_20240617R.zip?model=ROG%20STRIX%20X570-F%20GAMING",
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/01AUDIO/DRV_Audio_SZ_RTK_NS_6_W11_64_V6094111_20220928R.zip?model=ROG%20STRIX%20X570-F%20GAMING",
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/SocketAM4/ROG_CROSSHAIR_VIII_DARK_HERO/AISuite3_DIP_SystemInformation_EzUpdate_v3.03.36.zip?model=ROG%20STRIX%20X570-F%20GAMING",
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/14Utilities/ArmouryCrateInstallTool.zip?model=ROG%20STRIX%20X570-F%20GAMING",
+                            "https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-X570-F-GAMING-ASUS-5013.zip?model=ROG%20STRIX%20X570-F%20GAMING"
+                        }
+                    },
+                    new CheckBoxConfig { Text = "X570E", Urls = new List<string>
+                    {
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/04LAN/DRV_LAN_Realtek_8125_SZ-TSD_W11_64_V112596142022_20220908B.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/08WIRELESS/DRV_WiFi_Intel_All_SZ-TSD_W11_64_V2216003_20220909R.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/Socket%20AM5/PRIME%20X670-P/DRV_Chipset_AMD_AM5_SZ-TSD_W11_64_V60516221_20240617R.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/01AUDIO/DRV_Audio_SZ_RTK_NS_6_W11_64_V6094111_20220928R.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/02BT/DRV_Bluetooth_Intel_All_SZ-TSD_W11_64_V2216003_20220909R.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/SocketAM4/ROG_CROSSHAIR_VIII_DARK_HERO/AISuite3_DIP_SystemInformation_EzUpdate_v3.03.36.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/14Utilities/ArmouryCrateInstallTool.zip?model=ROG%20STRIX%20X570-E%20GAMING",
+                        "https://dlcdnets.asus.com/pub/ASUS/mb/BIOS/ROG-STRIX-X570-E-GAMING-ASUS-5013.zip?model=ROG%20STRIX%20X570-E%20GAMING"
+                    }
+                },
+                new CheckBoxConfig { Text = "STEAM", Urls = new List<string> { "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" } },
+                new CheckBoxConfig { Text = "EPIC", Urls = new List<string> { "https://epicgames-download1.akamaized.net/Builds/UnrealEngineLauncher/Installers/Win32/EpicInstaller-15.17.1.msi?launcherfilename=EpicInstaller-15.17.1-c5d141533b9c4748941fa8d9453c2b27.msi" } },
+                new CheckBoxConfig { Text = "CHROME", Urls = new List<string> { "https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B67D8FA1E-7803-716D-D81E-91E299A48F9F%7D%26lang%3Den-GB%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-statsdef_1%26brand%3DYTUH%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe" } },
+                new CheckBoxConfig { Text = "WINRAR", Urls = new List<string> { "https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-701.exe" } },
+                new CheckBoxConfig { Text = "SAMSUNG", Urls = new List<string> { "https://download.semiconductor.samsung.com/resources/software-resources/Samsung_Magician_Installer_Official_8.1.0.800.exe" } },
+                new CheckBoxConfig { Text = "CPUZ", Urls = new List<string> { "https://download.cpuid.com/cpu-z/cpu-z_2.10-rog-en.exe" } },
+                new CheckBoxConfig { Text = "GPUZ", Urls = new List<string> { "https://sg1-dl.techpowerup.com/files/25im0t8xkLEdCYL5Y_BS-w/1722284827/GPU-Z_ASUS_ROG_2.59.0.exe" } },
+                new CheckBoxConfig { Text = "CORETEMP", Urls = new List<string> { "https://www.alcpu.com/CoreTemp/Core-Temp-setup-v1.18.1.0.exe" } },
+                new CheckBoxConfig { Text = "RYZENMASTER", Urls = new List<string> { "https://download.amd.com/Desktop/amd-ryzen-master.exe" } },
+                new CheckBoxConfig { Text = "4060DRIVER", Urls = new List<string> { "https://download.gigabyte.com/FileList/Driver/551.86-desktop-win10-win11-64bit-international-dch-whql.exe?v=c9e5e21d3b1bbda38f85f5d64c148835" } },
+                new CheckBoxConfig { Text = "MINECRAFT", Urls = new List<string> { "https://launcher.mojang.com/download/MinecraftInstaller.msi?ref=mcnet" } },
+                new CheckBoxConfig { Text = "VISUALSTUDIO", Urls = new List<string> { "https://aka.ms/vs/17/release/vs_community.exe" } },
+            }
+                };
+                Save(filePath, defaultConfig);
+                return defaultConfig;
+            }
+
+            var json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<AppConfig>(json);
+        }
+
+        public class CheckBoxConfig
+        {
+            public string Text { get; set; }
+            public List<string> Urls { get; set; }
+        }
+
+        public static void Save(string filePath, AppConfig config)
+        {
+            var json = JsonConvert.SerializeObject(config, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
